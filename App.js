5036739ced5ce55335c2338ad2f844457b7a8bec
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-09-05 20:33:33 -0500

    :recycle: Update

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {
   StyleSheet,
   Text,
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   }
 })
 
-class randomBackground extends Component {
+class randomBackground extends React.Component {
   constructor(props) {
     super(props)
 
